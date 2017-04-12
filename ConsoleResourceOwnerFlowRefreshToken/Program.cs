8aaa479c407c4c029e7ef750508ee9f994724779
damienbod <damien_bod@hotmail.com>
2017-04-12 22:05:05 +0200

    init

---
+++ ConsoleResourceOwnerFlowRefreshToken/Program.cs
@@ -0,0 +1,118 @@
+﻿using IdentityModel;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleResourceOwnerFlowRefreshToken
+{
+    public class Program
+    {
+        static TokenClient _tokenClient;
+
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            Console.Title = "Console ResourceOwner Flow RefreshToken";
+
+            var disco = await DiscoveryClient.GetAsync("TODO_add_aut");
+            if (disco.IsError) throw new Exception(disco.Error);
+
+            _tokenClient = new TokenClient(
+                disco.TokenEndpoint,
+                "roclient",
+                "secret");
+
+            var response = await RequestTokenAsync();
+            //response.Show();
+
+            Console.ReadLine();
+
+            var refresh_token = response.RefreshToken;
+
+            while (true)
+            {
+                response = await RefreshTokenAsync(refresh_token);
+                ShowResponse(response);
+
+                Console.ReadLine();
+                await CallServiceAsync(response.AccessToken);
+
+                if (response.RefreshToken != refresh_token)
+                {
+                    refresh_token = response.RefreshToken;
+                }
+            }
+        }
+
+        static async Task<TokenResponse> RequestTokenAsync()
+        {
+            return await _tokenClient.RequestResourceOwnerPasswordAsync(
+                "bob",
+                "bob", 
+                "api1 api2.read_only offline_access");
+        }
+
+        private static async Task<TokenResponse> RefreshTokenAsync(string refreshToken)
+        {
+            Console.WriteLine("Using refresh token: {0}", refreshToken);
+
+            return await _tokenClient.RequestRefreshTokenAsync(refreshToken);
+        }
+
+        static async Task CallServiceAsync(string token)
+        {
+            var baseAddress = "https://localhist:5000";
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            client.SetBearerToken(token);
+            var response = await client.GetStringAsync("identity");
+
+            Console.WriteLine("\n\nService claims:");
+            Console.WriteLine(JArray.Parse(response));
+        }
+
+        private static void ShowResponse(TokenResponse response)
+        {
+            if (!response.IsError)
+            {
+                Console.WriteLine("Token response:");
+                Console.WriteLine(response.Json);
+
+                if (response.AccessToken.Contains("."))
+                {
+                    Console.WriteLine("\nAccess Token (decoded):");
+
+                    var parts = response.AccessToken.Split('.');
+                    var header = parts[0];
+                    var claims = parts[1];
+
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(header))));
+                    Console.WriteLine(JObject.Parse(Encoding.UTF8.GetString(Base64Url.Decode(claims))));
+                }
+            }
+            else
+            {
+                if (response.ErrorType == ResponseErrorType.Http)
+                {
+                    Console.WriteLine("HTTP error: ");
+                    Console.WriteLine(response.Error);
+                    Console.WriteLine("HTTP status code: ");
+                    Console.WriteLine(response.HttpStatusCode);
+                }
+                else
+                {
+                    Console.WriteLine("Protocol error response:");
+                    Console.WriteLine(response.Json);
+                }
+            }
+        }
+    }
+}
