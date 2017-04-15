70049aa8f5603bb931a0911ec75a51c3fd3a86df
damienbod <damien_bod@hotmail.com>
2017-04-15 12:46:24 +0200

    adding logging to the console

--- ConsoleResourceOwnerFlowRefreshToken/Program.cs
+++ ConsoleResourceOwnerFlowRefreshToken/Program.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityModel;
 using IdentityModel.Client;
 using Newtonsoft.Json.Linq;
+using Serilog;
 using System;
 using System.Net.Http;
 using System.Text;
@@ -15,14 +16,21 @@ namespace ConsoleResourceOwnerFlowRefreshToken
 
         static async Task MainAsync()
         {
+            Log.Logger = new LoggerConfiguration()
+              .MinimumLevel.Verbose()
+              .Enrich.FromLogContext()
+              .WriteTo.Seq("http://localhost:5341")
+              .WriteTo.ColoredConsole()
+              .CreateLogger();
+
             var response = IdentityServer4Client.LoginAsync("damienbod", "damienbod").Result;
-  
-            Console.WriteLine($"GOT TOKENS FROM IDENTITYSERVER4: {response.AccessToken}");
+
+            Log.Logger.Information("GOT TOKENS FROM IDENTITYSERVER4: {AccessToken}", response.AccessToken);
 
             // GET DATA from the resource server
             await ResourceDataClient.GetDataAndDisplayInConsoleAsync(response.AccessToken);
 
-            Console.WriteLine($"GOT DATA FROM THE RESOURCE SERVER");
+            Log.Logger.Information("GOT DATA FROM THE RESOURCE SERVER");
 
             // Run an loop which gets refreshes the token every 3000 milliseconds
             await IdentityServer4Client.RunRefreshAsync(response, 3000);
