ac2a4f8f2d3d9fadf2d900cb1c02a4939bc3f662
damienbod <damien_bod@hotmail.com>
2018-09-23 18:58:19 +0200

    Updating logging

--- AspNetCoreResourceServer/Program.cs
+++ AspNetCoreResourceServer/Program.cs
@@ -1,18 +1,47 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 
 namespace AspNet5SQLite
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Debug()
+           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+           .Enrich.FromLogContext()
+           .Enrich.WithProperty("App", "AspNetCoreResourceServer")
+           .WriteTo.RollingFile("logs/log-{Date}.txt")
+           .WriteTo.Seq("http://localhost:5341")
+           .CreateLogger();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            try
+            {
+                Log.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+        public static IWebHost BuildWebHost(string[] args) =>
+              WebHost.CreateDefaultBuilder(args)
+              .UseStartup<Startup>()
+                  .UseSerilog() // <-- Add this line
+                  .Build();
+
     }
 }
