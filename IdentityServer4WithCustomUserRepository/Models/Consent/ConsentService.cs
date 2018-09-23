3485b0275d19e6408b447c401d2a32b8b2006d8c
damienbod <damien_bod@hotmail.com>
2018-09-23 18:33:09 +0200

    Updating to bootstrap 4, add localization defaults

--- IdentityServer4WithCustomUserRepository/Models/Consent/ConsentService.cs
+++ IdentityServer4WithCustomUserRepository/Models/Consent/ConsentService.cs
@@ -126,18 +126,20 @@ namespace CustomIdentityServer4.Models
         private ConsentViewModel CreateConsentViewModel(
             ConsentInputModel model, string returnUrl, 
             AuthorizationRequest request, 
-            Client client, Resources resources)
+            Client client, IdentityServer4.Models.Resources resources)
         {
-            var vm = new ConsentViewModel();
-            vm.RememberConsent = model?.RememberConsent ?? true;
-            vm.ScopesConsented = model?.ScopesConsented ?? Enumerable.Empty<string>();
+            var vm = new ConsentViewModel
+            {
+                RememberConsent = model?.RememberConsent ?? true,
+                ScopesConsented = model?.ScopesConsented ?? Enumerable.Empty<string>(),
 
-            vm.ReturnUrl = returnUrl;
+                ReturnUrl = returnUrl,
 
-            vm.ClientName = client.ClientName;
-            vm.ClientUrl = client.ClientUri;
-            vm.ClientLogoUrl = client.LogoUri;
-            vm.AllowRememberConsent = client.AllowRememberConsent;
+                ClientName = client.ClientName,
+                ClientUrl = client.ClientUri,
+                ClientLogoUrl = client.LogoUri,
+                AllowRememberConsent = client.AllowRememberConsent
+            };
 
             vm.IdentityScopes = resources.IdentityResources.Select(x => CreateScopeViewModel(x, vm.ScopesConsented.Contains(x.Name) || model == null)).ToArray();
             vm.ResourceScopes = resources.ApiResources.SelectMany(x => x.Scopes).Select(x => CreateScopeViewModel(x, vm.ScopesConsented.Contains(x.Name) || model == null)).ToArray();
--- IdentityServer4WithCustomUserRepository/Startup.cs
+++ IdentityServer4WithCustomUserRepository/Startup.cs
@@ -7,6 +7,13 @@ using QuickstartIdentityServer;
 using System.Security.Cryptography.X509Certificates;
 using System.IO;
 using Serilog;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using CustomIdentityServer4.Resources;
+using Microsoft.Extensions.Options;
 
 namespace CustomIdentityServer4
 {
@@ -41,7 +48,44 @@ namespace CustomIdentityServer4
         {
             var cert = new X509Certificate2(Path.Combine(_environment.ContentRootPath, "damienbodserver.pfx"), "");
 
-            services.AddMvc();
+            services.AddSingleton<LocService>();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                        {
+                            new CultureInfo("en-US"),
+                            new CultureInfo("de-CH"),
+							new CultureInfo("fr-CH"),
+							new CultureInfo("it-CH")
+                        };
+
+                    options.DefaultRequestCulture = new RequestCulture(culture: "de-CH", uiCulture: "de-CH");
+                    options.SupportedCultures = supportedCultures;
+                    options.SupportedUICultures = supportedCultures;
+
+                    var providerQuery = new LocalizationQueryProvider
+                    {
+                        QureyParamterName = "ui_locales"
+                    };
+
+                    options.RequestCultureProviders.Insert(0, providerQuery);
+                });
+				
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization(options =>
+                {
+                    options.DataAnnotationLocalizerProvider = (type, factory) =>
+                    {
+                        var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+                        return factory.Create("SharedResource", assemblyName.Name);
+                    };
+                });
+				
             services.AddIdentityServer(options =>
                 {
                     options.Events.RaiseSuccessEvents = true;
@@ -73,6 +117,9 @@ namespace CustomIdentityServer4
                 app.UseExceptionHandler("/Home/Error");
             }
 
+			var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+			
             app.UseStaticFiles();
             app.UseIdentityServer();
 
